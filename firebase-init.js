447a436f78a25d626286e52bea96becabeb78ac4
nikkal-d <akirotato2@gmail.com>
2025-12-01 00:39:05 +0200

    Update firebase-init.js

--- firebase-init.js
+++ firebase-init.js
@@ -1,20 +1,22 @@
 import { initializeApp } from "https://www.gstatic.com/firebasejs/10.12.0/firebase-app.js";
+import { getAnalytics } from "https://www.gstatic.com/firebasejs/10.12.0/firebase-analytics.js";
 import { getFirestore } from "https://www.gstatic.com/firebasejs/10.12.0/firebase-firestore.js";
 import { getStorage } from "https://www.gstatic.com/firebasejs/10.12.0/firebase-storage.js";
 import { getAuth } from "https://www.gstatic.com/firebasejs/10.12.0/firebase-auth.js";
 
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
 const firebaseConfig = {
   apiKey: "AIzaSyBGt4Lsvk0ouISEtNbDbFirdY-XqmbHeSo",
   authDomain: "photobook-studio-b1064.firebaseapp.com",
   projectId: "photobook-studio-b1064",
- storageBucket: "photobook-studio-b1064.appspot.com",
+  storageBucket: "photobook-studio-b1064.appspot.com",
   messagingSenderId: "287968637580",
   appId: "1:287968637580:web:42311227a5df7b79ab81a9",
   measurementId: "G-75L68JM346"
 };
 
-
 export const app = initializeApp(firebaseConfig);
+export const analytics = getAnalytics(app);
 export const db = getFirestore(app);
 export const storage = getStorage(app);
 export const auth = getAuth(app);
