761d0a342c9cad6d89823c73fbff17415dca030e
nikkal-d <akirotato2@gmail.com>
2025-12-03 23:27:47 +0200

    Update loadFromFirebase.js

--- loadFromFirebase.js
+++ loadFromFirebase.js
@@ -1,8 +1,21 @@
 import { db } from "./firebase-init.js";
-import { doc, getDoc } from "https://www.gstatic.com/firebasejs/10.12.0/firebase-firestore.js";
+import {
+  doc,
+  getDoc
+} from "https://www.gstatic.com/firebasejs/10.12.0/firebase-firestore.js";
 
-export async function loadPhotobook(id){
-  const snap = await getDoc(doc(db, "photobooks", id));
-  if(!snap.exists()) throw new Error("Not found");
-  return snap.data();
+export async function loadPhotobook(docId) {
+  const ref = doc(db, "photobooks", docId);
+  const snap = await getDoc(ref);
+
+  if (!snap.exists()) {
+    throw new Error("Το photobook δεν βρέθηκε.");
+  }
+
+  const data = snap.data();
+  // επιστρέφουμε τις πληροφορίες (τίτλος, σελίδες, κλπ)
+  return {
+    id: snap.id,
+    ...data
+  };
 }
