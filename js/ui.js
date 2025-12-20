f7627a480901ecce7ade8f61aa9eefed64086ed4
nikkal-d <akirotato2@gmail.com>
2025-12-20 10:23:34 +0200

    Update ui.js

--- js/ui.js
+++ js/ui.js
@@ -1,16 +1,15 @@
-import { addText, addImageFromFile, zoom } from "./core.js";
+import { addText, addImageFromFile, zoom, fitToScreen } from "./core.js";
 
-document.getElementById("btnAddText").onclick = () => addText();
+document.getElementById("btnAddText").onclick = addText;
 
-document.getElementById("btnAddImage").onclick = () => {
+document.getElementById("btnAddImage").onclick = () =>
   document.getElementById("imageInput").click();
-};
 
 document.getElementById("imageInput").onchange = e => {
-  if (e.target.files[0]) {
-    addImageFromFile(e.target.files[0]);
-  }
+  const f = e.target.files[0];
+  if (f) addImageFromFile(f);
 };
 
 document.getElementById("btnZoomIn").onclick = () => zoom(0.1);
 document.getElementById("btnZoomOut").onclick = () => zoom(-0.1);
+document.getElementById("btnFit").onclick = fitToScreen;
