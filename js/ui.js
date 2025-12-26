2f6f1aa3bee88c1d2390aebbc394f437aac28184
nikkal-d <akirotato2@gmail.com>
2025-12-26 23:09:44 +0200

    Update ui.js

--- js/ui.js
+++ js/ui.js
@@ -1,21 +1,19 @@
 // ui.js
-import { initCanvas, addText, addImageFromFile } from "./core.js";
+import { initCanvas, addText, addImage, exportFlipbook } from "./core.js";
 
 window.addEventListener("DOMContentLoaded", () => {
   initCanvas();
 
-  const textBtn = document.getElementById("addTextBtn");
-  const imageInput = document.getElementById("imageInput");
-
-  textBtn?.addEventListener("click", () => {
+  document.getElementById("addTextBtn").onclick = () => {
     console.log("🟢 Add Text clicked");
     addText();
-  });
+  };
 
-  imageInput?.addEventListener("change", e => {
-    if (e.target.files[0]) {
-      addImageFromFile(e.target.files[0]);
-      e.target.value = "";
-    }
-  });
+  document.getElementById("imageInput").onchange = e => {
+    if (e.target.files[0]) addImage(e.target.files[0]);
+  };
+
+  document.getElementById("exportFlipbookBtn").onclick = () => {
+    exportFlipbook();
+  };
 });
