abd4ddf224b5b0bf7e5105a48b1532ae2e3c6dee
nikkal-d <akirotato2@gmail.com>
2025-12-30 00:33:30 +0200

    Update ui.js

--- js/ui.js
+++ js/ui.js
@@ -1,37 +1,38 @@
+// js/ui.js
 import {
   initCanvas,
   addText,
   addImageFromFile,
-  addPage,
-  prevPage,
-  nextPage,
-  setZoom,
+  zoomIn,
+  zoomOut,
   resetZoom,
   setPageSize,
+  addPage,
+  nextPage,
+  prevPage,
   exportFlipbook
 } from "./core.js";
 
 window.addEventListener("DOMContentLoaded", () => {
   initCanvas();
 
-  document.getElementById("addTextBtn")?.onclick = addText;
+  document.getElementById("addTextBtn")?.addEventListener("click", addText);
 
   document.getElementById("imageInput")?.addEventListener("change", e => {
     if (e.target.files[0]) addImageFromFile(e.target.files[0]);
   });
 
-  document.getElementById("addPageBtn")?.onclick = addPage;
-  document.getElementById("prevPageBtn")?.onclick = prevPage;
-  document.getElementById("nextPageBtn")?.onclick = nextPage;
-
-  document.getElementById("zoomInBtn")?.onclick = () => setZoom(0.1);
-  document.getElementById("zoomOutBtn")?.onclick = () => setZoom(-0.1);
-  document.getElementById("zoomResetBtn")?.onclick = resetZoom;
-  document.getElementById("zoomFitBtn")?.onclick = resetZoom;
+  document.getElementById("zoomInBtn")?.addEventListener("click", zoomIn);
+  document.getElementById("zoomOutBtn")?.addEventListener("click", zoomOut);
+  document.getElementById("zoomResetBtn")?.addEventListener("click", resetZoom);
 
   document.getElementById("pageSizeSelect")?.addEventListener("change", e => {
     setPageSize(e.target.value);
   });
 
-  document.getElementById("exportFlipBtn")?.onclick = exportFlipbook;
+  document.getElementById("addPageBtn")?.addEventListener("click", addPage);
+  document.getElementById("nextPageBtn")?.addEventListener("click", nextPage);
+  document.getElementById("prevPageBtn")?.addEventListener("click", prevPage);
+
+  document.getElementById("exportFlipBtn")?.addEventListener("click", exportFlipbook);
 });
