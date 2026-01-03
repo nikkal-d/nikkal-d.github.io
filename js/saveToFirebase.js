83e86abeb619fd6bf583fc97525e5e3c47d450c0
nikkal-d <akirotato2@gmail.com>
2026-01-03 23:35:05 +0200

    Update saveToFirebase.js

--- js/saveToFirebase.js
+++ js/saveToFirebase.js
@@ -2,6 +2,20 @@
 import { db, storage } from "./firebase-init.js";
 import { doc, setDoc, getDoc } from "https://www.gstatic.com/firebasejs/10.12.0/firebase-firestore.js";
 import { ref, uploadString, getDownloadURL } from "https://www.gstatic.com/firebasejs/10.12.0/firebase-storage.js";
+import { getStorage, ref, uploadBytes, getDownloadURL } from "https://www.gstatic.com/firebasejs/10.7.1/firebase-storage.js";
+
+const storage = getStorage();
+
+export async function uploadImageFile(file) {
+  const path = `images/${Date.now()}_${file.name}`;
+  const imageRef = ref(storage, path);
+
+  await uploadBytes(imageRef, file);
+  const url = await getDownloadURL(imageRef);
+
+  return url;
+}
+
 
 export async function saveProject(projectId, pages) {
   await setDoc(doc(db, "projects", projectId), {
