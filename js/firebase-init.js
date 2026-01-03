6945589204ba9accf2b073f0cd455e234937615a
nikkal-d <akirotato2@gmail.com>
2026-01-04 00:47:12 +0200

    Update firebase-init.js

--- js/firebase-init.js
+++ js/firebase-init.js
@@ -1,19 +1,19 @@
-
 // js/firebase-init.js
-import { initializeApp } from "https://www.gstatic.com/firebasejs/10.12.0/firebase-app.js";
-import { getFirestore } from "https://www.gstatic.com/firebasejs/10.12.0/firebase-firestore.js";
-import { getStorage } from "https://www.gstatic.com/firebasejs/10.12.0/firebase-storage.js";
+import { initializeApp } from "https://www.gstatic.com/firebasejs/10.12.5/firebase-app.js";
+import { getFirestore } from "https://www.gstatic.com/firebasejs/10.12.5/firebase-firestore.js";
+import { getAuth } from "https://www.gstatic.com/firebasejs/10.12.5/firebase-auth.js";
+import { getStorage } from "https://www.gstatic.com/firebasejs/10.12.5/firebase-storage.js";
 
 const firebaseConfig = {
-  apiKey: "AIzaSyBGt4Lsvk0ouISEtNbDbFirdY-XqmbHeSo",
+  apiKey: "AIzaSyA7_s-RdUl55RJZi1_QD_w4La-0jv0IOLA",
   authDomain: "photobook-studio-b1064.firebaseapp.com",
   projectId: "photobook-studio-b1064",
   storageBucket: "photobook-studio-b1064.firebasestorage.app",
-  messagingSenderId: "287968637580",
-  appId: "1:287968637580:web:42311227a5df7b79ab81a9",
-  measurementId: "G-75L68JM346"
+  messagingSenderId: "275371063372",
+  appId: "1:275371063372:web:92af7bba76a5d61373bc1b"
 };
 
 export const app = initializeApp(firebaseConfig);
 export const db = getFirestore(app);
+export const auth = getAuth(app);
 export const storage = getStorage(app);
