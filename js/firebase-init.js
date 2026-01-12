7eccce7bd29937b8e92fdead466a4fdbe4cd6fe1
nikkal-d <akirotato2@gmail.com>
2026-01-13 01:39:43 +0200

    Update firebase-init.js

--- js/firebase-init.js
+++ js/firebase-init.js
@@ -5,13 +5,13 @@ import { getFirestore } from "https://www.gstatic.com/firebasejs/10.12.2/firebas
 import { getStorage } from "https://www.gstatic.com/firebasejs/10.12.2/firebase-storage.js";
 
 export const firebaseConfig = {
-  apiKey: "AIzaSyD65Khx_U2kKa-zzJeG9PJ51e_BOT4OKX0",
+  apiKey: "AIzaSyBGt4Lsvk0ouISEtNbDbFirdY-XqmbHeSo",
   authDomain: "photobook-studio-b1064.firebaseapp.com",
   projectId: "photobook-studio-b1064",
-  // IMPORTANT: bucket name is usually *.appspot.com
-  storageBucket: "photobook-studio-b1064.appspot.com",
-  messagingSenderId: "892812056520",
-  appId: "1:892812056520:web:a7de52b53d4795dcbb2376",
+  storageBucket: "photobook-studio-b1064.firebasestorage.app",
+  messagingSenderId: "287968637580",
+  appId: "1:287968637580:web:42311227a5df7b79ab81a9",
+  measurementId: "G-75L68JM346"
 };
 
 export const app = initializeApp(firebaseConfig);
