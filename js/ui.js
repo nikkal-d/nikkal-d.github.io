b49d1ae0d31956e5d7731922cbf65a9f2b6b3795
nikkal-d <akirotato2@gmail.com>
2026-01-18 23:22:01 +0200

    Update ui.js

--- js/ui.js
+++ js/ui.js
@@ -140,9 +140,10 @@ document.addEventListener("DOMContentLoaded", async () => {
   if (multiplier) exportJPG(parseFloat(multiplier));
 });
   on("exportPdfBtn", "click", () => exportPDF());
- on("exportFlipbookBtn", "click", () => {
-    previewFlipbook(); // Αυτό πλέον κάνει το "Export" με το PDF μέσα
-});
+  on("exportFlipBtn", "click", async () => {
+    const dir = $("flipDirSelect")?.value || "horizontal";
+    await exportFlipbook({ direction: dir });
+  });
   on("previewFlipBtn", "click", async () => {
     const dir = $("flipDirSelect")?.value || "horizontal";
     await previewFlipbook({ direction: dir });
