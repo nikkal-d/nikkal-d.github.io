6cae86255bd8b0413fb6f9e815bba270e1b619be
nikkal-d <akirotato2@gmail.com>
2026-02-06 00:40:05 +0200

    Update core.js

--- js/core.js
+++ js/core.js
@@ -1074,7 +1074,7 @@ export async function exportFlipbook() {
   </html>`;
 
   frame.srcdoc = html;
-  modal.style.display = \"block\";
+  modal.style.display = "block";
 }
 
 // Ορισμός των exports ΜΙΑ ΦΟΡΑ στο τέλος
